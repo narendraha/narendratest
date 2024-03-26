20624035d6f5f15fb127afeeea9cd1a09a4dc913
dnmurthy <murthy.dirisala@vectramind.com>
2024-03-26 03:55:12 +0000

    Changed Landing Page menu to work dynamically

--- src/components/Layout/Layout3.jsx
+++ src/components/Layout/Layout3.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import FooterStyle3 from '../Footer/FooterStyle3';
 import Header from '../Header';
+import ChatBot from '../ChatBot';
 
 export default function Layout3() {
   return (
     <div id='al_main_landing'>
       <Header logoSrc="/images/alfredlogo.svg" variant="cs_heading_color" />
       <Outlet />
+      <ChatBot />
       <FooterStyle3 />
     </div>
   );

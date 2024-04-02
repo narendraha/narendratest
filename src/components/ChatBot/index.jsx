7b0c4d2e9ec899157525b1f5f312432b1cd5ae9a
Hemang Jiwnani <hemang.jiwnani@vectramind.com>
2024-04-02 11:37:58 +0000

    Added ChatBot Integration UI & Frontend for Alfred

--- src/components/ChatBot/index.jsx
+++ src/components/ChatBot/index.jsx
@@ -7,8 +7,24 @@ export default function ChatBot() {
     <>
       <div className='al_chatbot'>
         <Card>
-          <CardBody>
-              Chat need to be rendered here
+          <CardBody className='d-flex flex-column'>
+            <div className='flex-grow-1'>
+              <div className='scrolldiv'>
+                Chat need to be rendered here
+              </div>
+            </div>
+            <div className='cs_mainsearch al_chatfooter p-3' style={{ backgroundColor: "#F1F4F9" }}>
+              <form action="#">
+                <i className='icon_alfred_search' style={{ height: "auto" }}></i>
+                <input
+                  type="text"
+                  placeholder="Ask a question"
+                />
+                <i className='icon_alfred_close'></i>
+                <i className='icon_alfred_sendmsg' style={{ height: "auto" }}></i>
+              </form>
+              <div className='al_note pt-1'>Disclaimer: Not a medical advice</div>
+            </div>
           </CardBody>
         </Card>
         {/* <img src={chatBot} alt="bot" /> */}
--- src/components/Section/ContactInfoSection/index.jsx
+++ src/components/Section/ContactInfoSection/index.jsx
@@ -18,7 +18,7 @@ export default function ContactInfoSection({ sectionTitle }) {
         <div className="col-xl-4">
           <IconBoxStyle11
             title="Email"
-            subTitle="hellocallcenter@gmail.com"
+            subTitle="helloalfred@gmail.com"
             iconSrc="/images/contact/icon_2.svg"
           />
         </div>

e8498d4d2b8f8a636ebb12fd46750af5077c582e
Murthy <murthy.dirisala@vectramind.com>
2024-07-16 10:57:41 +0530

    designalignments

--- src/components/Pages/HomeEducationalBot.jsx
+++ src/components/Pages/HomeEducationalBot.jsx
@@ -98,7 +98,6 @@ export default function HomeEducationalBot() {
                       <style>
                         body {
                             font-family: Poppins;
-                            line-height: 1.8;
                         }
                         ol {
                             margin - top: 5px;

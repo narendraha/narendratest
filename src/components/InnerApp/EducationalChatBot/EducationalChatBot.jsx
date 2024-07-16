ec7f428e84a8b7ad438a1fd3854c1638b3739e31
Murthy <murthy.dirisala@vectramind.com>
2024-07-16 18:46:44 +0530

    designalignments

--- src/components/InnerApp/EducationalChatBot/EducationalChatBot.jsx
+++ src/components/InnerApp/EducationalChatBot/EducationalChatBot.jsx
@@ -145,7 +145,7 @@ export default function EducationalChatBot(props) {
                         <div className="d-flex align-items-center py-1" style={{ backgroundColor: "#ffffff" }}>
                             <div className="d-flex align-items-center">
                                 <img src={Chatbot} alt="botimage" className="ps-3" />
-                                <div className="ps-2" style={{lineHeight: 1.1}}>
+                                <div className="ps-2" style={{ lineHeight: 1.1 }}>
                                     <div className="fw-medium">ChatBot</div>
                                     <small className="text-success text-small">Online</small>
                                 </div>
@@ -207,28 +207,9 @@ export default function EducationalChatBot(props) {
                                                         <div>{value}</div> :
                                                         <div dangerouslySetInnerHTML={{ __html: value }} />}
                                                     {key === "alfred" && (
-                                                        <p className="mb-0 mt-1">
-                                                            <Icon
-                                                                icon="iconamoon:like-light"
-                                                                width="1.5em"
-                                                                height="1.5em"
-                                                                onClick={() => handleAction(index, 'like', value)} // Handle like action
-                                                                style={{
-                                                                    cursor: 'pointer',
-                                                                    color: selectedIcons[index]?.reaction === 'like' ? 'green' : '', // Apply green color if selected
-                                                                }}
-                                                            />
-                                                            <Icon
-                                                                icon="iconamoon:dislike-light"
-                                                                width="1.5em"
-                                                                height="1.5em"
-                                                                className="mx-2"
-                                                                onClick={() => handleAction(index, 'dislike', value)} // Handle dislike action
-                                                                style={{
-                                                                    cursor: 'pointer',
-                                                                    color: selectedIcons[index]?.reaction === 'dislike' ? 'red' : '', // Apply red color if selected
-                                                                }}
-                                                            />
+                                                        <p className="mb-0 mt-2 al_chatfeedbackactions">
+                                                            <i className={"icon_alfred_like pointer me-3 " + (selectedIcons[index]?.reaction === 'like' ? 'like' : '')} onClick={() => handleAction(index, 'like', value)}></i>
+                                                            <i className={"icon_alfred_dislike pointer me-3 " + (selectedIcons[index]?.reaction === 'dislike' ? 'text-danger mt-0' : '')} onClick={() => handleAction(index, 'dislike', value)}></i>
                                                         </p>
                                                     )}
                                                 </Col>

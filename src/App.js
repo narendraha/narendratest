1be4c1c8cf5ed7f788395c740ecc55245e970c61
purnima9vm <153587965+purnima9vm@users.noreply.github.com>
2024-08-16 10:30:15 +0530

    implemented loading for redux-persist and also resolved double loading issue for default pages

--- src/App.js
+++ src/App.js
@@ -19,29 +19,6 @@ export default function App() {
     }
   }, [])
 
-  // useEffect(() => {
-  //   const token = localStorage.getItem("token");
-  //   let isAuth = token ? true : false
-  //   dispatch(setAuthenticationRequest(isAuth))
-
-  //   try {
-  //     if (token) {
-  //       const decoded = jwtDecode(token);
-  //       // Check if token is expired
-  //       const currentTime = Date.now() / 1000;
-  //       if (decoded?.exp < currentTime) {
-  //         // Redirect to sign-up page if token is expired
-  //         localStorage.clear();
-  //         sessionStorage.clear();
-  //         navigate("signin");
-  //       }
-  //     }
-  //   } catch (error) {
-  //     console.log("Error decoding token:", error);
-  //     navigate("signin"); // Redirect to sign-in page if token decoding fails
-  //   }
-  // }, [navigate]);
-
   return (
     <AllRoutes
       authenticated={isAuthenticated}
--- src/components/MainLayout/Loading.js
+++ src/components/MainLayout/Loading.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import loading from '../../images/loadinganim.gif';
-import { useSelector } from 'react-redux';
+import { setLoading } from '../../store/UtilityCallFunction/slice';
 
-export default function Loading() {
+export default function Loading(props) {
+  const dispatch = useDispatch();
+  let { storeLoading, rehydrating } = props || false;
 
   const layOutLoading = useSelector((state) => state?.utilityCallFunctionSlice?.isLoading);
-  
+
+  const isLoading = layOutLoading || storeLoading || rehydrating;
+
+  useEffect(() => {
+    if (rehydrating) {
+      dispatch(setLoading(true));
+    } else if (!layOutLoading && !storeLoading) {
+      dispatch(setLoading(false));
+    }
+  }, [rehydrating, layOutLoading, storeLoading, dispatch]);
+
   return (
     <>
-      {layOutLoading && < div className="al_mainLoading" >
-        <img src={loading} alt="loading" />
-      </div >}
+      {isLoading && (
+        <div className="al_mainLoading">
+          <img src={loading} alt="loading" />
+        </div>
+      )}
     </>
-  )
+  );
 }

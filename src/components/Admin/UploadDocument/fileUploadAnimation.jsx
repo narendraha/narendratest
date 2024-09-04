0c7654f8fdb6fdb47fd286f1d4a250406ac4030f
Murthy <murthy.dirisala@vectramind.com>
2024-09-04 18:32:11 +0530

    uploaddocumentchanges

--- src/components/Admin/UploadDocument/fileUploadAnimation.jsx
+++ src/components/Admin/UploadDocument/fileUploadAnimation.jsx
@@ -44,58 +44,67 @@ export const FileUploadAnimation = ({ props }) => {
     return (
         <>
             {!showFinalCheck && fileUpload !== "" && (
-                <div className='al_upload_steps'>
-                    <Card className={`mb-3 ${completedStep >= 1 ? 'completedstep' : ''}`}>
-                        <CardBody>
-                            <div className='d-flex align-items-center justify-content-between'>
-                                <div className='d-flex align-items-center'>
-                                    {renderIconOrNumber(1)}
-                                    <div className="line"></div>
-                                    <div>
-                                        <small className='mb-1 fw-semibold text-dark'>Step 1</small>
-                                        <div>Uploading File</div>
+                <div className="final-check-overlay">
+                    <div className="final-check-container">
+                        <div className='al_upload_steps'>
+                            <Card className={`mb-3 ${completedStep >= 1 ? 'completedstep' : ''}`}>
+                                <CardBody>
+                                    <div className='d-flex align-items-center justify-content-between'>
+                                        <div className='d-flex align-items-center'>
+                                            {renderIconOrNumber(1)}
+                                            <div className="line"></div>
+                                            <div>
+                                                <small className='mb-1 fw-semibold text-dark'>Step 1</small>
+                                                <div>Uploading File</div>
+                                            </div>
+                                        </div>
+                                        <i className='icon_alfred_uploaddocument'></i>
                                     </div>
-                                </div>
-                                <i className='icon_alfred_uploaddocument'></i>
-                            </div>
-                        </CardBody>
-                    </Card>
-                    <Card className={`mb-3 ${completedStep >= 2 ? 'completedstep' : ''}`}>
-                        <CardBody>
-                            <div className='d-flex align-items-center justify-content-between'>
-                                <div className='d-flex align-items-center'>
-                                    {renderIconOrNumber(2)}
-                                    <div className="line"></div>
-                                    <div>
-                                        <small className='mb-1 fw-semibold text-dark'>Step 2</small>
-                                        <div>Processing Data</div>
+                                </CardBody>
+                            </Card>
+                            <Card className={`mb-3 ${completedStep >= 2 ? 'completedstep' : ''}`}>
+                                <CardBody>
+                                    <div className='d-flex align-items-center justify-content-between'>
+                                        <div className='d-flex align-items-center'>
+                                            {renderIconOrNumber(2)}
+                                            <div className="line"></div>
+                                            <div>
+                                                <small className='mb-1 fw-semibold text-dark'>Step 2</small>
+                                                <div>Processing Data</div>
+                                            </div>
+                                        </div>
+                                        <i className='icon_alfred_sync'></i>
                                     </div>
-                                </div>
-                                <i className='icon_alfred_sync'></i>
-                            </div>
-                        </CardBody>
-                    </Card>
-                    <Card className={`mb-3 ${completedStep >= 3 ? 'completedstep' : ''}`}>
-                        <CardBody>
-                            <div className='d-flex align-items-center justify-content-between'>
-                                <div className='d-flex align-items-center'>
-                                    {renderIconOrNumber(3)}
-                                    <div>
-                                        <small className='mb-1 fw-semibold text-dark'>Step 3</small>
-                                        <div>Finalizing Upload</div>
+                                </CardBody>
+                            </Card>
+                            <Card className={`mb-3 ${completedStep >= 3 ? 'completedstep' : ''}`}>
+                                <CardBody>
+                                    <div className='d-flex align-items-center justify-content-between'>
+                                        <div className='d-flex align-items-center'>
+                                            {renderIconOrNumber(3)}
+                                            <div>
+                                                <small className='mb-1 fw-semibold text-dark'>Step 3</small>
+                                                <div>Finalizing Upload</div>
+                                            </div>
+                                        </div>
+                                        <i className='icon_alfred_circle_check_solid'></i>
                                     </div>
-                                </div>
-                                <i className='icon_alfred_circle_check_solid'></i>
-                            </div>
-                        </CardBody>
-                    </Card>
+                                </CardBody>
+                            </Card>
+                        </div>
+                    </div>
                 </div>
             )}
 
             {showFinalCheck && (
                 <div className="final-check-overlay">
                     <div className="final-check-container">
-                        <div className="big-checkmark">&#10004;</div>
+                        <Card className='mb-0 al_cardview h-auto'>
+                            <CardBody className="p-4">
+                                <div className="big-checkmark mx-auto">&#10004;</div>
+                                <h6 className='mt-3'>File Uploaded Successfully</h6>
+                            </CardBody>
+                        </Card>
                     </div>
                 </div>
             )}

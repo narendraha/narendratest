59720f854cc73b0186a845e3f2260db99760817b
purnima9vm <153587965+purnima9vm@users.noreply.github.com>
2024-11-08 16:04:39 +0530

    upload document view logic changes

--- src/components/Utilities/DocumentViewer.jsx
+++ src/components/Utilities/DocumentViewer.jsx
@@ -1,45 +1,56 @@
 import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-// import { setMenuOrPdfExpend } from '../../../store/EducationaChatBot/slice';
+import { useDispatch, useSelector } from 'react-redux';
 import { Spinner } from 'reactstrap'; // Use any spinner or loading component you prefer
-
+import { getActionTypes } from '../../_mock/internalJsControl';
+import { setActionTypeAndActionData } from '../../store/UtilityCallFunction/slice';
+import img from "../../images/doctorbot.png"
 
 export const DocumentViewer = React.memo((props) => {
-    
+    const dispatch = useDispatch();
+
     const [loading, setLoading] = useState(true);
 
     const actionData = useSelector((state) => (state?.utilityCallFunctionSlice?.actionData));
 
-    let { link, fileType } = actionData;
+    let { link, fileType } = actionData || "";
 
     const handleIframeLoad = () => {
         setLoading(false);
     };
 
+    const handleCloseDocView = () => {
+        dispatch(setActionTypeAndActionData({ actionType: getActionTypes.UNSELECT, actionData: null }));
+    }
+
     return (
         <React.Fragment>
+            {/* Show loading spinner until iframe is loaded */}
+            {loading && (
+                <div className="text-center my-4 d-flex align-items-center h-100 justify-content-center">
+                    <Spinner color="primary" />
+                    <p className='mb-0'>{`Loading ${fileType}...`}</p>
+                </div>
+            )}
 
-                {/* Show loading spinner until iframe is loaded */}
-                {loading && (
-                    <div className="text-center my-4 d-flex align-items-center h-100 justify-content-center">
-                        <Spinner color="primary" />
-                        <p className='mb-0'>{`Loading ${fileType}...`}</p>
-                    </div>
-                )}
-
-                {/* PDF Viewer using iframe */}
-                {link ? (
+            {/* PDF Viewer using iframe */}
+            {link !== null ? (
+                <>
+                    <i
+                        className="icon_alfred_close pointer text-end"
+                        onClick={handleCloseDocView}
+                    ></i>
                     <iframe
-                        src={link}
+                        src={img}
                         width="100%"
                         height="auto"
                         title="PDF Viewer"
                         style={{ border: 'none', height: "100%" }}
                         onLoad={handleIframeLoad} // Triggered when iframe content is loaded
                     ></iframe>
-                ) : (
-                    <p className='mb-0'>No PDF available to display.</p>
-                )}
+                </>
+            ) : (
+                <p className='mb-0'>No PDF available to display.</p>
+            )}
         </React.Fragment>
     );
 });
